6e00ee00da1e52c23fe3e5019ace12bdc50a445d
ABDULLAH-GARIBOV <garibovabdullah4@gmail.com>
2026-05-20 01:28:48 +0300

    REST API (Arka uç) servisi tamamlandı ve .gitignore eklendi

---
+++ StudentApi/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var student = new
+            {
+                StudentNumber = "B211200565", 
+                FirstName = "Abdullah",
+                LastName = "Garibov"
+            };
+
+            return Ok(student);
+        }
+    }
+}
---
+++ StudentWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
